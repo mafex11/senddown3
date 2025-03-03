b947a47b7080ec2adc120efd9365fe74f521090b
Sudhanshu Pandit <sudhanshuk1140@gmail.com>
2025-03-03 08:50:38 +0530

    Update page.tsx

--- src/app/CreateRoom/page.tsx
+++ src/app/CreateRoom/page.tsx
@@ -5,6 +5,20 @@ import { useState } from 'react';
 import axios from 'axios';
 import FileRoom from '../../components/FileRoom';
 
+import { NavigationMenuDemo } from '../../components/NavigationMenu';
+import { Button } from '../../components/ui/button';
+import Link from 'next/link';
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
+
 export default function CreateRoom() {
   const [roomId, setRoomId] = useState('');
   const [inputRoomId, setInputRoomId] = useState('');
@@ -20,6 +34,37 @@ export default function CreateRoom() {
 
   return (
     <div className="min-h-screen p-8">
+      <div className="min-h-screen flex flex-col">
+      
+      {/* Navigation Bar */}
+      <nav className="fixed  top-4 left-0 right-0 w-3/4 mx-auto max-w-7xl z-50 rounded-xl  shadow-md p-4">
+        <div className="max-w-7xl mx-auto flex items-center justify-between">
+          <h1 className="text-2xl font-bold hidden lg:block ">SendDown</h1>
+          <NavigationMenuDemo />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem onClick={() => setTheme("light")}>
+                    Light
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    Dark
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme("system")}>
+                    System
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+          
+      </nav>
+      </div>
       {!roomId ? (
         <div className="max-w-md mx-auto space-y-4">
           <h1 className="text-3xl font-bold text-center">
